4b5adc3f74d76abf5be9e94a2b4a4875e36d89b9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-09-30 14:42:10 -0700

    bump version to 2

--- build_and_package.sh
+++ build_and_package.sh
@@ -2,18 +2,16 @@
 
 read -p "Is the kernel repo up to date? " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Update it and run again!"
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Update it and run again!"
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
-read -p "Is the firmware repo up to date? Copied in the new abl if needed?" -n 1 -r
+read -p "Is the firmware repo up to date? Copied in the new abl if needed? " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Update it and run again!"
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Update it and run again!"
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
 ./build_kernel.sh
@@ -21,4 +19,3 @@ cp output/wlan.ko userspace/usr/comma
 cp output/snd*.ko userspace/usr/comma/sound/
 ./build_system.sh
 ./package_ota.sh
-
