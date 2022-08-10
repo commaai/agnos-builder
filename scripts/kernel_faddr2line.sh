46fae683ebd14e3bbece7ca9212a8e06b9cb7dcb
Robbe Derks <robbe.derks@gmail.com>
2022-08-10 14:03:35 +0200

    add kernel crash debug script

---
+++ scripts/kernel_faddr2line.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"/..
+TOOLS=$ROOT_DIR/tools
+KERNEL_DIR=$ROOT_DIR/agnos-kernel-sdm845
+CROSS_COMPILE=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-
+
+$KERNEL_DIR/scripts/faddr2line $KERNEL_DIR/out/vmlinux $1
+
