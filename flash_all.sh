72935defbcf4f283faab33a8bb3afd8de2c3a86c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-02-23 19:22:37 -0800

    flash all OTA partitions in flash_all.sh

--- flash_all.sh
+++ flash_all.sh
@@ -1,7 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
+
+for part in aop xbl xbl_config devcfg; do
+  fastboot flash ${part}_a $DIR/agnos-firmware/$part.bin
+  fastboot flash ${part}_b $DIR/agnos-firmware/$part.bin
+done
 
 ./flash_bootloader.sh
 ./flash_kernel.sh
