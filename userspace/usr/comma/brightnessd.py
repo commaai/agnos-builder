6d5978b6971f13161f6b26d37f9063e69dbe91ff
Comma Device <device@comma.ai>
2023-05-07 18:37:06 -0500

    brightnessd: ignore some panels

--- userspace/usr/comma/brightnessd.py
+++ userspace/usr/comma/brightnessd.py
@@ -12,6 +12,10 @@ MAX_PERCENT = 90
 MIN_PERCENT = 30
 HOURLY_PERC_DECREASE = 5
 
+IGNORED_PANELS = [
+  "dsi_mate10_lite_video_display",
+]
+
 def read(path: str) -> int:
   try:
     with open(path) as f:
@@ -22,9 +26,19 @@ def read(path: str) -> int:
 
 if __name__ == "__main__":
 
+  # detect panel type
+  with open("/sys/module/msm_drm/parameters/dsi_display0") as f:
+    d = f.read().strip()
+    panel = d.split(":")[0]
+
+  print(f"Detected panel '{panel}'")
+  if panel in IGNORED_PANELS:
+    print("panel is ignored, exiting")
+    exit(0)
+
+  # brightness loop
   last_perc = None
   last_off_ts = time.monotonic()
-
   while True:
     try:
       # sample brightness and backlight power
