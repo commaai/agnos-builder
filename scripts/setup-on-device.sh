3aa3c8b4f5c5becabb0d005dfcc63d54b0a6f008
Comma Device <device@comma.ai>
2023-05-09 18:49:10 -0500

    copy

--- scripts/setup-on-device.sh
+++ scripts/setup-on-device.sh
@@ -11,18 +11,16 @@ fi
 
 sudo mount -o rw,remount /
 
-# symlink /usr/comma/
 echo "symlink /usr/comma"
 sudo rm -rf /usr/comma
 sudo ln -snf $ROOT/userspace/usr/comma/ /usr/comma
 
-# symlink services
-echo "symlink systemd services"
+echo "cp systemd services"
 for s in $(ls $ROOT/userspace/files/*.service); do
   service=$(basename $s)
   echo "- $service"
   sudo rm -f /lib/systemd/system/$service
-  sudo ln -sf $ROOT/userspace/files/*.service /lib/systemd/system/
+  sudo cp $ROOT/userspace/files/$service /lib/systemd/system/
 done
 sudo $ROOT/userspace/services.sh
 
