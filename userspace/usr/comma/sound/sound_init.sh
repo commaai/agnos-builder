3668cf59c623ecbb1a17dc5f5430641cea742fb8
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-05-11 21:54:10 -0700

    fix rare sound init failure (#138)
    * test
    
    * executable
    
    * wait for sound card
    
    * check that too
    
    * update
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>

--- userspace/usr/comma/sound/sound_init.sh
+++ userspace/usr/comma/sound/sound_init.sh
@@ -5,6 +5,12 @@
 insmod /usr/comma/sound/snd-soc-wcd9xxx.ko
 insmod /usr/comma/sound/snd-soc-sdm845.ko
 
+echo "waiting for sound card to come online"
+while [ ! -d /proc/asound/sdm845tavilsndc ] || [ "$(cat /proc/asound/card0/state 2> /dev/null)" != "ONLINE" ] ; do
+  sleep 0.01
+done
+echo "sound card online"
+
 /usr/comma/sound/tinymix set "SEC_MI2S_RX Audio Mixer MultiMedia1" 1
 /usr/comma/sound/tinymix set "MultiMedia1 Mixer TERT_MI2S_TX" 1
 
