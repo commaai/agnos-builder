8ebf85df68d4e7a40c4a09c0891179a720f1cb0d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-06-30 14:37:01 -0700

    anomalous touch logger (#147)
    * anomalous touch logger
    
    * service
    
    * add evdev
    
    * rename
    
    * Update userspace/files/phantom_touch_logger.service

--- userspace/services.sh
+++ userspace/services.sh
@@ -25,6 +25,7 @@ systemctl enable ssh-param-watcher.service
 systemctl enable home.mount
 systemctl enable logrotate-hourly.timer
 systemctl enable set_time.service
+systemctl enable phantom_touch_logger.service
 
 # Disable some of our services
 systemctl disable serial-hostname.service
