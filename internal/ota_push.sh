44bd68359ced8b8b29a8e4840e8d558e62666b19
Cameron Clough <cameronjclough@gmail.com>
2023-08-03 11:46:02 +0100

    Update internal/ota_push.sh

--- internal/ota_push.sh
+++ internal/ota_push.sh
@@ -34,7 +34,7 @@ process_file() {
     local FILE_NAME="$NAME-$HASH.img.$EXT"
     local CLOUD_PATH="https://$DATA_ACCOUNT.blob.core.windows.net/$DATA_CONTAINER/$FILE_NAME"
 
-    echo "Copying $NAME to the cloud..."
+    echo "Copying $NAME $EXT to the cloud..."
     azcopy cp --overwrite=false $OTA_DIR/$FILE_NAME "$CLOUD_PATH?$DATA_SAS_TOKEN"
     echo "  $CLOUD_PATH"
   done
