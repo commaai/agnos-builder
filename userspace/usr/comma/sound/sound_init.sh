c65e7edfaac18850049fe5ec492826708be8a9d9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-08-04 22:26:35 -0700

    fix stereo mic setup (#162)

--- userspace/usr/comma/sound/sound_init.sh
+++ userspace/usr/comma/sound/sound_init.sh
@@ -13,6 +13,7 @@ echo "sound card online"
 
 /usr/comma/sound/tinymix set "SEC_MI2S_RX Audio Mixer MultiMedia1" 1
 /usr/comma/sound/tinymix set "MultiMedia1 Mixer TERT_MI2S_TX" 1
+/usr/comma/sound/tinymix set "TERT_MI2S_TX Channels" Two
 
 # setup the amplifier registers
 /usr/local/pyenv/shims/python /usr/comma/sound/amplifier.py
