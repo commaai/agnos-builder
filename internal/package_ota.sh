deda4eaef434943af518ae65606f7b6bdd06943e
Cameron Clough <cameronjclough@gmail.com>
2023-08-11 15:03:52 +0100

    tool path

--- internal/package_ota.sh
+++ internal/package_ota.sh
@@ -43,7 +43,7 @@ process_file() {
 
     echo "Optimizing sparse system image..."
     local FILE_OPTIMIZED=/tmp/system-optimized.img
-    simg2dontcare $FILE $FILE_OPTIMIZED
+    $ROOT/tools/simg2dontcare $FILE $FILE_OPTIMIZED
 
 
     # echo "Creating system casync files"
