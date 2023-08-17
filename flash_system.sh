34de4ce783ce40868fbede8186891a021abec2ba
Cameron Clough <cameronjclough@gmail.com>
2023-08-17 12:15:06 +0100

    cleaner

--- flash_system.sh
+++ flash_system.sh
@@ -12,14 +12,12 @@ NO_COLOR='\033[0m'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-TOOLS_DIR="$DIR/tools"
-
 # Flash system
 fastboot --set-active=a
 
 if [ -d $HOME/openpilot/provisioning ]; then
   DIMG=$(mktemp)
-  $TOOLS_DIR/simg2dontcare.py $OUTPUT_DIR/$ROOTFS_IMAGE $DIMG
+  $DIR/tools/simg2dontcare.py $OUTPUT_DIR/$ROOTFS_IMAGE $DIMG
   fastboot erase system_a
   fastboot flash system_a $DIMG
 else
