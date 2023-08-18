0680f3ca9aa7369ea3e01d07ba6eea99c3068f4c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-08-18 11:34:59 -0700

    don't care build

--- build_system.sh
+++ build_system.sh
@@ -16,7 +16,8 @@ OUTPUT_DIR="$DIR/output"
 ROOTFS_DIR="$BUILD_DIR/agnos-rootfs"
 ROOTFS_IMAGE="$BUILD_DIR/system.img.raw"
 ROOTFS_IMAGE_SIZE=10G
-SPARSE_IMAGE="$BUILD_DIR/system.img"
+SPARSE_IMAGE="$OUTPUT_DIR/system.img"
+MISSING_CHUNKS_IMAGE="$OUTPUT_DIR/system-missing-chunks.img"
 
 # Create temp dir if non-existent
 mkdir -p $BUILD_DIR $OUTPUT_DIR
@@ -87,7 +88,13 @@ sudo umount -l $ROOTFS_DIR
 
 # Sparsify
 echo "Sparsify image"
-img2simg $ROOTFS_IMAGE $SPARSE_IMAGE
-mv $SPARSE_IMAGE $OUTPUT_DIR
+TMP_SPARSE="$(mktemp)"
+img2simg $ROOTFS_IMAGE $TMP_SPARSE
+mv $TMP_SPARSE $SPARSE_IMAGE
+
+# Make image with missing chunks
+TMP_MISSING="$(mktemp)"
+$DIR/tools/simg2dontcare.py $SPARSE_IMAGE $TMP_MISSING
+mv $TMP_MISSING $MISSING_CHUNKS_IMAGE
 
 echo "Done!"
