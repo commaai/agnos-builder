93255573dd70d8e4f0a5c70fb6d6ede612142159
Cameron Clough <cameronjclough@gmail.com>
2023-08-20 16:56:46 +0100

    wip

--- internal/package_ota.py
+++ internal/package_ota.py
@@ -18,8 +18,7 @@ def checksum(fn: str) -> str:
   return subprocess.check_output(["sha256sum", fn]).decode().split()[0]
 
 
-def process_file(fn, name, sparse=False, full_check=True, has_ab=True, alt=None) -> dict:
-  fn = os.path.join(OUTPUT_DIR, fn)
+def process_file(remote_url, fn, name, sparse=False, full_check=True, has_ab=True, alt=None) -> dict:
   hash_raw = hash = checksum(fn)
   size = os.path.getsize(fn)
 
@@ -29,6 +28,8 @@ def process_file(fn, name, sparse=False, full_check=True, has_ab=True, alt=None)
       hash_raw = checksum(f.name)
       size = os.path.getsize(f.name)
 
+  # TODO: compress
+
   ret = {
     "name": name,
     "hash": hash,
@@ -37,13 +38,14 @@ def process_file(fn, name, sparse=False, full_check=True, has_ab=True, alt=None)
     "sparse": sparse,
     "full_check": full_check,
     "has_ab": has_ab,
+    "url": f"{remote_url}/{fn.name}.xz",
   }
 
   if alt is not None:
+    hash = checksum(alt)
     ret["alt"] = {
-      "url": "",
-      "hash": "",
-      "size": 0,
+      "url": f"{remote_url}/{alt.name}.xz",
+      "hash": hash,
     }
 
   return ret
@@ -56,13 +58,13 @@ if __name__ == "__main__":
   ]
   for remote_url, output_fn in configs:
     files = [
-      process_file(OUTPUT_DIR / "boot.img", "boot"),
-      process_file(FIRMWARE_DIR / "abl.bin", "abl"),
-      process_file(FIRMWARE_DIR / "xbl.bin", "xbl"),
-      process_file(FIRMWARE_DIR / "xbl_config.bin", "xbl_config"),
-      process_file(FIRMWARE_DIR / "devcfg.bin", "devcfg"),
-      process_file(FIRMWARE_DIR / "aop.bin", "aop"),
-      process_file(OUTPUT_DIR / "system.img", "system", sparse=True, full_check=False),
+      process_file(remote_url, OUTPUT_DIR / "boot.img", "boot"),
+      process_file(remote_url, FIRMWARE_DIR / "abl.bin", "abl"),
+      process_file(remote_url, FIRMWARE_DIR / "xbl.bin", "xbl"),
+      process_file(remote_url, FIRMWARE_DIR / "xbl_config.bin", "xbl_config"),
+      process_file(remote_url, FIRMWARE_DIR / "devcfg.bin", "devcfg"),
+      process_file(remote_url, FIRMWARE_DIR / "aop.bin", "aop"),
+      process_file(remote_url, OUTPUT_DIR / "system.img", "system", sparse=True, full_check=False, alt=OUTPUT_DIR / "system-skip-chunks.img"),
     ]
     with open(OTA_OUTPUT_DIR / output_fn, "w") as f:
       f.write(json.dumps(files, indent=2))
