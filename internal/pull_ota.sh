d85f69dbeab525aac271a3a0d6b85c17349a8b8e
Cameron Clough <cameronjclough@gmail.com>
2023-08-21 16:46:42 +0100

    switch back

--- internal/pull_ota.sh
+++ internal/pull_ota.sh
@@ -5,7 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 # Constants
-OUTPUT_DIR="$DIR/../output2"
+OUTPUT_DIR="$DIR/../output"
 
 if [ "$1" == "" ]; then
   echo "Supply the URL to the OTA JSON as first argument!"
