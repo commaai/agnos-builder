05ad7ff97fbb88bcc2f02b28d753ebe2e7508217
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-09-05 19:30:09 -0700

    setup color prompt

--- userspace/install_extras.sh
+++ userspace/install_extras.sh
@@ -6,3 +6,6 @@ apt-get update && apt-get install -y --no-install-recommends \
   irqtop \
   ripgrep \
   nfs-common
+
+# color prompt
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/comma/.bashrc
