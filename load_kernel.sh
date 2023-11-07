2635dccdb63017ce70ca4c8e924138b536bfd7e0
Greg Hogan <gregjhogan@gmail.com>
2023-11-07 11:27:58 -0800

    update kernel load script to support passing in device name

--- load_kernel.sh
+++ load_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"
 
-DEVICE=tici-ethernet
+DEVICE=${DEVICE:-tici-ethernet}
 
 scp output/boot.img $DEVICE:/tmp/
 ssh $DEVICE "sudo dd if=/tmp/boot.img of=/dev/disk/by-partlabel/boot_a && sudo dd if=/tmp/boot.img of=/dev/disk/by-partlabel/boot_b && sudo reboot"
