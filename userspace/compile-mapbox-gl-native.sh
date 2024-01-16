00dbb0650f505765baad88d6e7a3d4cc496e82f5
Andrei Radulescu <andi.radulescu@gmail.com>
2024-01-16 10:23:00 +0000

    temp g++-11 for mapbox-gl-native

--- userspace/compile-mapbox-gl-native.sh
+++ userspace/compile-mapbox-gl-native.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+g++ --version
+dpkg --list | grep gcc | grep -v lib
+
+apt-get install --no-install-recommends -yq g++-11
+
+g++-11 --version
+dpkg --list | grep g++ | grep -v lib
+
+export CXX=g++-11
+
 # Build mapbox-gl-native
 cd /tmp
 git clone --recursive https://github.com/andiradulescu/mapbox-gl-native.git
