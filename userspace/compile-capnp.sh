7520703117ca05979b420fdbcb932400a0808fdb
Andrei Radulescu <andi.radulescu@gmail.com>
2024-01-16 10:23:33 +0000

    bump capnproto to 1.0.2

--- userspace/compile-capnp.sh
+++ userspace/compile-capnp.sh
@@ -2,7 +2,7 @@
 
 # Install capnproto
 cd /tmp
-VERSION=0.8.0
+VERSION=1.0.2
 wget https://capnproto.org/capnproto-c++-${VERSION}.tar.gz
 tar xvf capnproto-c++-${VERSION}.tar.gz
 cd capnproto-c++-${VERSION}
