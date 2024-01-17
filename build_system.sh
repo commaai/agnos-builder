f0538c7e34ba78e36cc349db50ff1bc6a5078ce6
Andrei Radulescu <andi.radulescu@gmail.com>
2024-01-17 09:49:58 +0200

    Build main system image again

--- build_system.sh
+++ build_system.sh
@@ -44,10 +44,11 @@ fi
 # Start docker build
 echo "Building image"
 export DOCKER_CLI_EXPERIMENTAL=enabled
-# docker build -f Dockerfile.agnos -t agnos-builder $DIR
+docker build -f Dockerfile.agnos -t agnos-builder $DIR
 # docker build -f Dockerfile.agnos -t agnos-compiler-capnp --target agnos-compiler-capnp $DIR
-docker build -f Dockerfile.agnos -t agnos-compiler-mapbox-gl-native --target agnos-compiler-mapbox-gl-native $DIR
-exit 0
+# docker build -f Dockerfile.agnos -t agnos-compiler-mapbox-gl-native --target agnos-compiler-mapbox-gl-native $DIR
+# docker build -f Dockerfile.agnos -t agnos-compiler-ffmpeg --target agnos-compiler-ffmpeg $DIR
+# exit 0
 
 # Create filesystem ext4 image
 echo "Creating empty filesystem"
