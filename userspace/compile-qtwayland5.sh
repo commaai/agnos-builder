7cf97ce7fe30e8673e0035c2ab1258b2216ec3ec
Andrei Radulescu <andi.radulescu@gmail.com>
2024-01-22 23:54:53 +0200

    Perform qtwayland5 compilation within dockerfile (#192)
    * separate out qtwayland5 compilation
    
    * qt5-qmake qtbase5-dev qtbase5-dev-tools already in qt5-default
    
    ---------
    
    Co-authored-by: Justin Newberry <justin@comma.ai>

---
+++ userspace/compile-qtwayland5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# Patched qtwayland that outputs a fixed screen size
+# Clone qtwayland submodule, checkout 5.12.9 (5.12.8 leaks timers, see https://bugreports.qt.io/browse/QTBUG-82914), apply patch, qmake, make
+
+cd /tmp
+git clone --branch v5.12.9 https://github.com/qt/qtwayland.git
+cd qtwayland
+
+git apply /tmp/agnos/patch
+
+mkdir /tmp/build && cd /tmp/build
+qmake /tmp/qtwayland
+
+export MAKEFLAGS="-j$(nproc)"
+make
+
+checkinstall -yD --install=no --pkgversion="5.12.8" --pkgname=qtwayland5 --pkgarch=arm64 --replaces=qtwayland5,libqt5waylandclient5,libqt5waylandcompositor5
+mv qtwayland5*.deb /tmp/qtwayland5.deb
