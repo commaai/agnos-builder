458283a35c0694fef1e445e95389533f1b157b76
Andrei Radulescu <andi.radulescu@gmail.com>
2024-01-22 22:18:39 +0200

    qt5-qmake qtbase5-dev qtbase5-dev-tools already in qt5-default

--- userspace/compile-qtwayland5.sh
+++ userspace/compile-qtwayland5.sh
@@ -3,8 +3,6 @@
 # Patched qtwayland that outputs a fixed screen size
 # Clone qtwayland submodule, checkout 5.12.9 (5.12.8 leaks timers, see https://bugreports.qt.io/browse/QTBUG-82914), apply patch, qmake, make
 
-apt update && apt install -y qt5-qmake qtbase5-dev qtbase5-dev-tools
-
 cd /tmp
 git clone --branch v5.12.9 https://github.com/qt/qtwayland.git
 cd qtwayland
