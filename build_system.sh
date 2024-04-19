adf49b319494f24bcbe4be0d1ac7e66004fd6e41
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-04-18 19:41:52 -0700

    all good now

--- build_system.sh
+++ build_system.sh
@@ -27,7 +27,7 @@ if ! ls $OUTPUT_DIR/*.ko >/dev/null 2>&1; then
   echo "kernel modules missing. run ./build_kernel.sh first"
   exit 1
 fi
-cp $OUTPUT_DIR/wlan.ko $DIR/userspace/usr/comma
+#cp $OUTPUT_DIR/wlan.ko $DIR/userspace/usr/comma
 cp $OUTPUT_DIR/snd*.ko $DIR/userspace/usr/comma/sound/
 
 # Download Ubuntu Base if not done already
