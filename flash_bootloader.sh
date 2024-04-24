d9c8ef05827a56ad95c0a7d6d5cb0bebbd6fe3c1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-04-24 13:37:02 -0700

    bump edl

--- flash_bootloader.sh
+++ flash_bootloader.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
-# Log colors
 GREEN="\033[0;32m"
 NO_COLOR='\033[0m'
 
-# Make sure we're in the correct spot
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
@@ -12,4 +10,4 @@ echo "Flashing bootloader..."
 tools/edl w abl_a output/abl.elf
 tools/edl w abl_b output/abl.elf
 
-echo -e "${GREEN}Done!"
+echo -e "${GREEN}Flashed abl_a and abl_b!${NO_COLOR}"
--- flash_kernel.sh
+++ flash_kernel.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
 
-# Log colors
 GREEN="\033[0;32m"
 NO_COLOR='\033[0m'
 
-# Make sure we're in the correct spot
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-# Flash bootloader
 echo "Flashing kernel..."
 tools/edl w boot_a output/boot.img
 tools/edl w boot_b output/boot.img
 
-echo -e "${GREEN}Done!"
+echo -e "${GREEN}Flashed boot_a and boot_b!${NO_COLOR}"
