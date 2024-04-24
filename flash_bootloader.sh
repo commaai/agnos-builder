f67000022c094fd7c369cc1d51303a40af7c6ac8
Hoang Bui <47828508+bongbui321@users.noreply.github.com>
2024-04-24 16:39:33 -0400

    Fastboot -> QDL flashing (#201)
    * edl
    
    * only edl
    
    * cleaner
    
    * restore DIR
    
    * better name
    
    * export
    
    * edl setup in extract_tools
    
    * back to root dir
    
    * update + wait for edl merge
    
    * good
    
    * fix
    
    * fix
    
    * update checkpoint
    
    * edl_flash.sh
    
    * edl command setup script
    
    * remove edl_flash
    
    * remove edl json
    
    * simple wrapper
    
    * fix set slot
    
    * bump edl
    
    ---------
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- flash_bootloader.sh
+++ flash_bootloader.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -e
 
-# Config
-OUTPUT_DIR="output"
-BOOTLOADER_IMAGE="abl.elf"
-
-# Log colors
 GREEN="\033[0;32m"
 NO_COLOR='\033[0m'
 
-# Make sure we're in the correct spot
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-# Flash bootloader
-fastboot flash abl_a $OUTPUT_DIR/$BOOTLOADER_IMAGE
-fastboot flash abl_b $OUTPUT_DIR/$BOOTLOADER_IMAGE
+echo "Flashing bootloader..."
+tools/edl w abl_a output/abl.elf
+tools/edl w abl_b output/abl.elf
 
-echo -e "${GREEN}Done!"
+echo -e "${GREEN}Flashed abl_a and abl_b!${NO_COLOR}"
