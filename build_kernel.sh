bf327e0c094c9735b229cc3be218a42281e976bc
Andrei Radulescu <andi.radulescu@gmail.com>
2024-07-17 18:32:16 +0300

    build kernel in container

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,68 +4,105 @@ DEFCONFIG=tici_defconfig
 
 # Get directories and make sure we're in the correct spot to start the build
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-TOOLS=$DIR/tools
-TMP_DIR=/tmp/agnos-builder-tmp
-OUTPUT_DIR=$DIR/output
-BOOT_IMG=./boot.img
 cd $DIR
+
+if ! command -v docker &> /dev/null; then
+  echo "Docker is not installed. Please install Docker and try again."
+  echo "https://docs.docker.com/engine/install/"
+  echo "Don't forget to run `sudo usermod -aG docker $USER` after Docker installation is complete."
+  exit 1
+fi
+
+if ! command -v git &> /dev/null; then
+  echo "Git is not installed. Please install Git and try again."
+  echo "https://www.atlassian.com/git/tutorials/install-git"
+  exit 1
+fi
 
 # Clone kernel if not done already
 if git submodule status --cached agnos-kernel-sdm845/ | grep "^-"; then
   git submodule update --init agnos-kernel-sdm845
 fi
-cd agnos-kernel-sdm845
 
-# Build parameters
-ARCH=$(uname -m)
-if [ "$ARCH" != "arm64" ] && [ "$ARCH" != "aarch64" ]; then
-  export CROSS_COMPILE=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-
-  export CC=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-gcc
-  export LD=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-ld.bfd
-
-  $DIR/tools/extract_tools.sh
+# Setup kernel build container
+if ! docker inspect agnos-kernel &>/dev/null; then
+  echo "Building agnos-kernel docker image"
+  docker build -f Dockerfile.kernel -t agnos-kernel $DIR
 fi
-export ARCH=arm64
+echo "Starting agnos-kernel container"
+CONTAINER_ID=$(docker run -d --privileged -v $DIR:$DIR -w $DIR/agnos-kernel-sdm845 agnos-kernel)
 
-# these do anything?
-export KCFLAGS="-w"
+# Cleanup container on exit
+trap "echo \"Cleaning up container:\"; \
+docker container rm -f $CONTAINER_ID" EXIT
 
-# Load defconfig and build kernel
-echo "-- First make --"
-make $DEFCONFIG O=out
-echo "-- Second make: $(nproc --all) cores --"
-make -j$(nproc --all) O=out  # Image.gz-dtb
+# Actual build_kernel steps
+build_kernel() {
+  TOOLS=$DIR/tools
+  TMP_DIR=/tmp/agnos-builder-tmp
+  OUTPUT_DIR=$DIR/output
+  BOOT_IMG=./boot.img
 
-# Turn on if you want perf
-# LDFLAGS=-static make -j$(nproc --all) -C tools/perf
+  # Build parameters
+  ARCH=$(uname -m)
+  if [ "$ARCH" != "arm64" ] && [ "$ARCH" != "aarch64" ]; then
+    export CROSS_COMPILE=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-
+    export CC=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-gcc
+    export LD=$TOOLS/aarch64-linux-gnu-gcc/bin/aarch64-linux-gnu-ld.bfd
 
-# Copy over Image.gz-dtb
-mkdir -p $TMP_DIR
-cd $TMP_DIR
-cp $DIR/agnos-kernel-sdm845/out/arch/arm64/boot/Image.gz-dtb .
+    $DIR/tools/extract_tools.sh
+  fi
 
-# Make boot image
-$TOOLS/mkbootimg \
-  --kernel Image.gz-dtb \
-  --ramdisk /dev/null \
-  --cmdline "console=ttyMSM0,115200n8 quiet loglevel=3 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.selinux=permissive firmware_class.path=/lib/firmware/updates net.ifnames=0 dyndbg=\"\"" \
-  --pagesize 4096 \
-  --base 0x80000000 \
-  --kernel_offset 0x8000 \
-  --ramdisk_offset 0x8000 \
-  --tags_offset 0x100 \
-  --output $BOOT_IMG.nonsecure
+  # Build arm64 arch
+  export ARCH=arm64
 
-# le signing
-openssl dgst -sha256 -binary $BOOT_IMG.nonsecure > $BOOT_IMG.sha256
-openssl pkeyutl -sign -in $BOOT_IMG.sha256 -inkey $DIR/vble-qti.key -out $BOOT_IMG.sig -pkeyopt digest:sha256 -pkeyopt rsa_padding_mode:pkcs1
-dd if=/dev/zero of=$BOOT_IMG.sig.padded bs=2048 count=1
-dd if=$BOOT_IMG.sig of=$BOOT_IMG.sig.padded conv=notrunc
-cat $BOOT_IMG.nonsecure $BOOT_IMG.sig.padded > $BOOT_IMG
+  # Disable all warnings
+  export KCFLAGS="-w"
 
-# Copy to output dir
-mkdir -p $OUTPUT_DIR
-mv $BOOT_IMG $OUTPUT_DIR/
-cp $DIR/agnos-kernel-sdm845/out/techpack/audio/asoc/snd-soc-sdm845.ko $OUTPUT_DIR/
-cp $DIR/agnos-kernel-sdm845/out/techpack/audio/asoc/codecs/snd-soc-wcd9xxx.ko $OUTPUT_DIR/
-cp $DIR/agnos-kernel-sdm845/out/drivers/staging/qcacld-3.0/wlan.ko $OUTPUT_DIR/
+  # Load defconfig and build kernel
+  echo "-- First make --"
+  make $DEFCONFIG O=out
+  echo "-- Second make: $(nproc --all) cores --"
+  make -j$(nproc --all) O=out  # Image.gz-dtb
+
+  # Turn on if you want perf
+  # LDFLAGS=-static make -j$(nproc --all) -C tools/perf
+
+  # Copy over Image.gz-dtb
+  mkdir -p $TMP_DIR
+  cd $TMP_DIR
+  cp $DIR/agnos-kernel-sdm845/out/arch/arm64/boot/Image.gz-dtb .
+
+  # Make boot image
+  $TOOLS/mkbootimg \
+    --kernel Image.gz-dtb \
+    --ramdisk /dev/null \
+    --cmdline "console=ttyMSM0,115200n8 quiet loglevel=3 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.selinux=permissive firmware_class.path=/lib/firmware/updates net.ifnames=0 dyndbg=\"\"" \
+    --pagesize 4096 \
+    --base 0x80000000 \
+    --kernel_offset 0x8000 \
+    --ramdisk_offset 0x8000 \
+    --tags_offset 0x100 \
+    --output $BOOT_IMG.nonsecure
+
+  # le signing
+  openssl dgst -sha256 -binary $BOOT_IMG.nonsecure > $BOOT_IMG.sha256
+  openssl pkeyutl -sign -in $BOOT_IMG.sha256 -inkey $DIR/vble-qti.key -out $BOOT_IMG.sig -pkeyopt digest:sha256 -pkeyopt rsa_padding_mode:pkcs1
+  dd if=/dev/zero of=$BOOT_IMG.sig.padded bs=2048 count=1
+  dd if=$BOOT_IMG.sig of=$BOOT_IMG.sig.padded conv=notrunc
+  cat $BOOT_IMG.nonsecure $BOOT_IMG.sig.padded > $BOOT_IMG
+
+  # Copy to output dir
+  mkdir -p $OUTPUT_DIR
+  mv $BOOT_IMG $OUTPUT_DIR/
+  cp $DIR/agnos-kernel-sdm845/out/techpack/audio/asoc/snd-soc-sdm845.ko $OUTPUT_DIR/
+  cp $DIR/agnos-kernel-sdm845/out/techpack/audio/asoc/codecs/snd-soc-wcd9xxx.ko $OUTPUT_DIR/
+  cp $DIR/agnos-kernel-sdm845/out/drivers/staging/qcacld-3.0/wlan.ko $OUTPUT_DIR/
+}
+
+# Create host user in container (fixes namespace.so error)
+USERNAME=$(whoami)
+docker exec $CONTAINER_ID bash -c "useradd --uid $(id -u) -U -m $USERNAME"
+
+# Run build_kernel in container
+docker exec -u $USERNAME $CONTAINER_ID bash -c "export DEFCONFIG=$DEFCONFIG; export DIR=$DIR; $(declare -f build_kernel); build_kernel"
