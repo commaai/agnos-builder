63b8458285550411fbb024ceb9c4fc3c6f4137c3
ubuntu <170117454+coder351@users.noreply.github.com>
2024-07-18 06:27:42 -0400

    resolve issues

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -2,19 +2,23 @@
 
 echo "Installing python for openpilot"
 
-if ! command -v "uv" > /dev/null 2>&1; then
-  echo "installing uv..."
-  curl -LsSf https://astral.sh/uv/install.sh | sh
-  UV_BIN='$HOME/.cargo/env'
-  ADD_PATH_CMD=". \"$UV_BIN\""
-  eval $ADD_PATH_CMD
-fi
+echo "installing uv..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+UV_BIN='$HOME/.cargo/env'
+ADD_PATH_CMD=". \"$UV_BIN\""
+eval $ADD_PATH_CMD
 
 PYTHON_VERSION="3.11.4"
 if [ "$(uname -p)" == "aarch64" ]; then
-  uv python install 3.11.4
+  uv python install $PYTHON_VERSION
 else
   MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 uv python install --verbose $PYTHON_VERSION
 fi
-uv venv
-uv pip install --no-cache-dir --upgrade pip uv
+
+# uv requires virtual env either managed or system before installing dependencies
+uv venv --python-preference only-system
+# need to activate virtual env otherwise call to uv pip install throws error,
+# error: No virtual or system environment found for path ...
+source .venv/bin/activate
+# install dependencies using system python 
+uv pip install --python=$(which python) --no-cache-dir --upgrade pip uv
