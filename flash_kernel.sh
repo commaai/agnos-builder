703daf7a5c3308938bb3c1fb7cf23bd9b0ff105d
Andrei Radulescu <andi.radulescu@gmail.com>
2024-07-26 11:44:41 +0300

    update flash scripts

--- flash_kernel.sh
+++ flash_kernel.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-GREEN="\033[0;32m"
-NO_COLOR='\033[0m'
-
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 echo "Flashing kernel..."
-tools/edl w boot_a output/boot.img
-tools/edl w boot_b output/boot.img
+tools/edl w boot_a $DIR/output/boot.img
 
-echo -e "${GREEN}Flashed boot_a and boot_b!${NO_COLOR}"
+echo "Done!"
--- flash_system.sh
+++ flash_system.sh
@@ -5,7 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 echo "Flashing system..."
-tools/edl w system_a $DIR/output/system-skip-chunks.img
+tools/edl w system_a $DIR/output/system.img
 
 tools/edl setactiveslot a
 tools/edl setbootablestoragedrive 1
