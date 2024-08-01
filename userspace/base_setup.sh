6ee691ad227ff1fdd03b789b1561dab1f3c7e1a0
Andrei Radulescu <andi.radulescu@gmail.com>
2024-08-01 18:34:16 +0300

    no armhf [upload]

--- userspace/base_setup.sh
+++ userspace/base_setup.sh
@@ -8,9 +8,6 @@ HOST=comma
 touch /TICI
 touch /AGNOS
 
-# Add armhf as supported architecture
-dpkg --add-architecture armhf
-
 # Install packages
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -57,17 +54,22 @@ apt-get install --no-install-recommends -yq \
     chrony \
     cpuset \
     dfu-util \
+    dnsmasq-base \
     evtest \
     git \
     git-core \
     git-lfs \
     gdb \
+    hostapd \
     htop \
     i2c-tools \
     ifmetric \
     ifupdown \
     iptables-persistent \
+    iputils-ping \
+    isc-dhcp-client \
     jq \
+    kmod \
     landscape-common \
     libi2c-dev \
     libqmi-utils \
@@ -79,13 +81,16 @@ apt-get install --no-install-recommends -yq \
     libsqlite3-dev \
     libssl-dev \
     libffi-dev \
+    libgtk2.0-dev \
     llvm \
     nano \
     net-tools \
     nload \
     network-manager \
     nvme-cli \
+    openssh-server \
     openssl \
+    rsyslog \
     smartmontools \
     speedtest-cli \
     ssh \
@@ -100,6 +105,7 @@ apt-get install --no-install-recommends -yq \
     udhcpc \
     wget \
     wireless-tools \
+    wpasupplicant \
     zlib1g-dev
 
 rm -rf /var/lib/apt/lists/*
@@ -125,81 +131,76 @@ echo "comma ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -sf /bin/bash /bin/sh
 
 # Install necessary libs
-apt-get update -yq
-apt-get install --no-install-recommends -yq \
-    libacl1:armhf \
-    libasan6-armhf-cross \
-    libatomic1-armhf-cross \
-    libattr1:armhf \
-    libaudit1:armhf \
-    libblkid1:armhf \
-    libc6:armhf \
-    libc6-armhf-cross \
-    libc6-dev:armhf \
-    libc6-dev-armhf-cross \
-    libcairo2:armhf \
-    libcap2:armhf \
-    libdrm2:armhf \
-    libevdev2:armhf \
-    libexpat1:armhf \
-    libffi8:armhf \
-    libfontconfig1:armhf \
-    libfreetype6:armhf \
-    libgbm1:armhf \
-    libgcc-11-dev-armhf-cross \
-    libglib2.0-0t64:armhf \
-    libgomp1-armhf-cross \
-    libgudev-1.0-0:armhf \
-    libinput-bin:armhf \
-    libinput-dev:armhf \
-    libinput10:armhf \
-    libjpeg-dev:armhf \
-    libjpeg-turbo8:armhf \
-    libjpeg-turbo8-dev:armhf \
-    libjpeg8:armhf \
-    libjpeg8-dev:armhf \
-    libkmod2:armhf \
-    libmtdev1t64:armhf \
-    libpam0g:armhf \
-    libpam0g-dev:armhf \
-    libpcre3:armhf \
-    libpixman-1-0:armhf \
-    libpng16-16t64:armhf \
-    libselinux1:armhf \
-    libstdc++6:armhf \
-    libstdc++6-armhf-cross \
-    libubsan1-armhf-cross \
-    libudev-dev:armhf \
-    libudev1:armhf \
-    libuuid1:armhf \
-    libwacom9:armhf \
-    libx11-6:armhf \
-    libxau6:armhf \
-    libxcb-render0:armhf \
-    libxcb-shm0:armhf \
-    libxcb1:armhf \
-    libxdmcp6:armhf \
-    libxext6:armhf \
-    libxkbcommon0:armhf \
-    libxrender1:armhf \
-    linux-libc-dev:armhf \
-    linux-libc-dev-armhf-cross \
-    zlib1g:armhf \
-    libegl1 \
-    libegl-dev \
-    libgles1 \
-    libgles2 \
-    libgles-dev \
-    openssh-server \
-    dnsmasq-base \
-    isc-dhcp-client \
-    iputils-ping \
-    rsyslog \
-    kmod \
-    wpasupplicant \
-    hostapd \
-    libgtk2.0-dev \
-    libxml2:armhf \
+# apt-get update -yq
+# apt-get install --no-install-recommends -yq \
+
+# TODO: moving to agnos-display
+    # libegl1 \
+    # libegl-dev \
+    # libgles1 \
+    # libgles2 \
+    # libgles-dev \
+
+# old armhf
+    # libacl1:armhf \
+    # libasan6-armhf-cross \
+    # libatomic1-armhf-cross \
+    # libattr1:armhf \
+    # libaudit1:armhf \
+    # libblkid1:armhf \
+    # libc6:armhf \
+    # libc6-armhf-cross \
+    # libc6-dev:armhf \
+    # libc6-dev-armhf-cross \
+    # libcairo2:armhf \
+    # libcap2:armhf \
+    # libdrm2:armhf \
+    # libevdev2:armhf \
+    # libexpat1:armhf \
+    # libffi8:armhf \
+    # libfontconfig1:armhf \
+    # libfreetype6:armhf \
+    # libgbm1:armhf \
+    # libgcc-11-dev-armhf-cross \
+    # libglib2.0-0t64:armhf \
+    # libgomp1-armhf-cross \
+    # libgudev-1.0-0:armhf \
+    # libinput-bin:armhf \
+    # libinput-dev:armhf \
+    # libinput10:armhf \
+    # libjpeg-dev:armhf \
+    # libjpeg-turbo8:armhf \
+    # libjpeg-turbo8-dev:armhf \
+    # libjpeg8:armhf \
+    # libjpeg8-dev:armhf \
+    # libkmod2:armhf \
+    # libmtdev1t64:armhf \
+    # libpam0g:armhf \
+    # libpam0g-dev:armhf \
+    # libpcre3:armhf \
+    # libpixman-1-0:armhf \
+    # libpng16-16t64:armhf \
+    # libselinux1:armhf \
+    # libstdc++6:armhf \
+    # libstdc++6-armhf-cross \
+    # libubsan1-armhf-cross \
+    # libudev-dev:armhf \
+    # libudev1:armhf \
+    # libuuid1:armhf \
+    # libwacom9:armhf \
+    # libx11-6:armhf \
+    # libxau6:armhf \
+    # libxcb-render0:armhf \
+    # libxcb-shm0:armhf \
+    # libxcb1:armhf \
+    # libxdmcp6:armhf \
+    # libxext6:armhf \
+    # libxkbcommon0:armhf \
+    # libxrender1:armhf \
+    # libxml2:armhf \
+    # linux-libc-dev:armhf \
+    # linux-libc-dev-armhf-cross \
+    # zlib1g:armhf \
 
 # TODO: put these back when updating weston & removing old libwayland-1.9.0-1.deb
     # libwayland-client0:armhf \
