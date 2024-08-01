36ffe2318536664782e72e4c8e0673e757258ecf
Robin Reckmann <robin.reckmann@gmail.com>
2024-08-01 16:26:30 +0900

    Some more apt-get replacements

--- userspace/compile-modemmanager.sh
+++ userspace/compile-modemmanager.sh
@@ -7,16 +7,16 @@ PROVIDER_INFO_VERSION="20230416"
 
 cd /tmp
 
-apt update
-apt install -y --no-install-recommends automake autoconf build-essential cmake
+apt-fast update
+apt-fast install -y --no-install-recommends automake autoconf build-essential cmake
 
 # TODO: clean up these build time dependencies
-apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-wheel ninja-build
+apt-fast install -y --no-install-recommends python3 python3-pip python3-setuptools python3-wheel ninja-build
 pip3 install --user meson
 export PATH=$PATH:/root/.local/bin
 
 # build mobile-broadband-provider-info
-apt install -y --no-install-recommends xsltproc
+apt-fast install -y --no-install-recommends xsltproc
 git clone -b $PROVIDER_INFO_VERSION --depth 1 https://gitlab.gnome.org/GNOME/mobile-broadband-provider-info.git
 cd mobile-broadband-provider-info
 ./autogen.sh
@@ -25,7 +25,7 @@ make install
 
 # build libqmi
 cd /tmp
-apt install -y --no-install-recommends libgudev-1.0-dev gobject-introspection libgirepository1.0-dev help2man bash-completion
+apt-fast install -y --no-install-recommends libgudev-1.0-dev gobject-introspection libgirepository1.0-dev help2man bash-completion
 
 git clone -b $LIBQMI_VERSION --depth 1 https://gitlab.freedesktop.org/mobile-broadband/libqmi.git
 cd libqmi
