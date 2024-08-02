84298d4df17eeb33fc4b6e754bcb77f3b0340399
Robin Reckmann <robin.reckmann@gmail.com>
2024-08-02 18:28:46 +0900

    Apply pyenvtouv changes

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -e
 
-echo "Installing python for openpilot"
+echo "installing uv..."
 
-# Install pyenv
-export PYENV_ROOT="/usr/local/pyenv"
-curl https://pyenv.run | bash
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+export XDG_DATA_HOME="/usr/local"
+export CARGO_HOME="$XDG_DATA_HOME/.cargo"
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+eval ". $CARGO_HOME/env"
 
 PYTHON_VERSION="3.11.4"
+
+echo "Installing python for openpilot"
+
 if [ "$(uname -p)" == "aarch64" ]; then
-  pyenv install --verbose $PYTHON_VERSION
+  uv python install $PYTHON_VERSION
 else
-  MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 pyenv install --verbose $PYTHON_VERSION
+  MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 uv python install --verbose $PYTHON_VERSION
 fi
 
-echo "Setting global python version"
-pyenv global $PYTHON_VERSION
-
-pip3 install --no-cache-dir --upgrade pip uv
+# uv requires virtual env either managed or system before installing dependencies
+uv venv $XDG_DATA_HOME/venv --seed --python-preference only-managed --python=$PYTHON_VERSION
