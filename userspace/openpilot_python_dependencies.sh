65ad9a5cae8f99a86dd97876c8529e222374f9de
Robin Reckmann <robin.reckmann@gmail.com>
2024-08-09 16:45:31 +0900

    Use system python 3.12 and uv venv

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -1,21 +1,11 @@
 #!/bin/bash -e
 
-echo "Installing python for openpilot"
+export CARGO_HOME="$XDG_DATA_HOME/.cargo"
 
-# Install pyenv
-export PYENV_ROOT="/usr/local/pyenv"
-curl https://pyenv.run | bash
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+apt-fast update
+apt-fast install -yq --no-install-recommends python3-dev
 
-PYTHON_VERSION="3.11.4"
-if [ "$(uname -p)" == "aarch64" ]; then
-  pyenv install --verbose $PYTHON_VERSION
-else
-  MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 pyenv install --verbose $PYTHON_VERSION
-fi
+curl -LsSf https://astral.sh/uv/install.sh | sh
+eval ". $CARGO_HOME/env"
 
-echo "Setting global python version"
-pyenv global $PYTHON_VERSION
-
-pip3 install --no-cache-dir --upgrade pip uv
+uv venv $XDG_DATA_HOME/venv
