1837383640a658a3b2361cffe7b6ae76ff317bc0
ubuntu <170117454+coder351@users.noreply.github.com>
2024-08-10 08:15:24 -0400

    use uv python 3.12.3

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -e
-# install pyproject.toml dependencies
-apt-get install portaudio19-dev -y
-apt-get install opencl-headers -y
-apt-get install gcc-arm-none-eabi -y
 
 echo "installing uv..."
 
@@ -12,15 +8,7 @@ export CARGO_HOME="$XDG_DATA_HOME/.cargo"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 eval ". $CARGO_HOME/env"
 
-PYTHON_VERSION="3.11.4"
-
-echo "Installing python for openpilot"
-
-if [ "$(uname -p)" == "aarch64" ]; then
-  uv python install $PYTHON_VERSION
-else
-  MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 uv python install --verbose $PYTHON_VERSION
-fi
+PYTHON_VERSION="3.12.3"
 
 # uv requires virtual env either managed or system before installing dependencies
 uv venv $XDG_DATA_HOME/venv --seed --python-preference only-managed --python=$PYTHON_VERSION
