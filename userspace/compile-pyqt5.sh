ad4a912169bb1d62a9353ca0fc56a2c966c8866c
Robin Reckmann <robin.reckmann@gmail.com>
2024-08-11 08:46:25 +0900

    Add missing compile-pyqt5.sh

---
+++ userspace/compile-pyqt5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+# Use uv venv
+export XDG_DATA_HOME="/usr/local"
+export PATH="$XDG_DATA_HOME/.cargo/bin:$PATH"
+source $XDG_DATA_HOME/venv/bin/activate
+
+# Build PyQt5 wheel
+cd /tmp
+wget https://files.pythonhosted.org/packages/5c/46/b4b6eae1e24d9432905ef1d4e7c28b6610e28252527cdc38f2a75997d8b5/PyQt5-5.15.9.tar.gz
+tar xf PyQt5-5.15.9.tar.gz
+cd PyQt5-5.15.9 
+
+export MAKEFLAGS="-j$(nproc)"
+pip wheel -w . --verbose --config-settings --confirm-license= .
