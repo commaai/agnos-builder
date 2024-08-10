aa43d330d68b4e09270872ef88c6f18745e14724
Robin Reckmann <robin.reckmann@gmail.com>
2024-08-10 20:15:08 +0900

    Install uv python 3.12

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -7,4 +7,15 @@ apt-fast install -yq --no-install-recommends python3-dev
 curl -LsSf https://astral.sh/uv/install.sh | sh
 eval ". $CARGO_HOME/env"
 
-uv venv $XDG_DATA_HOME/venv
+PYTHON_VERSION="3.12"
+
+echo "Installing python for openpilot"
+
+if [ "$(uname -p)" == "aarch64" ]; then
+  uv python install $PYTHON_VERSION
+else
+  MAKEFLAGS="-j1" MAKE_OPTS="-j1" taskset --cpu-list 0 uv python install --verbose $PYTHON_VERSION
+fi
+
+# uv requires virtual env either managed or system before installing dependencies
+uv venv $XDG_DATA_HOME/venv --seed --python-preference only-managed --python=$PYTHON_VERSION
