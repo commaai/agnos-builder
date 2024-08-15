9c46cf7be906306decf0d672fae0be2d4358248d
ubuntu <170117454+coder351@users.noreply.github.com>
2024-08-15 04:34:25 -0400

    rebuild pyqt5

--- userspace/compile-pyqt5.sh
+++ userspace/compile-pyqt5.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-export rebuildpyqt5='1'
-
-if [[ "$(rebuildpyqt5)" == '1' ]]; then
+rebuildpyqt5=1
+if [[ "$rebuildpyqt5" -eq "1" ]]; then
   echo "rebuilding pyqt5"
   touch ./userspace/files/PyQt5-5.15.9-cp38-abi3-manylinux_2_17_aarch64.whl
 fi
