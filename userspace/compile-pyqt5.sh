ece511ebf3aad3fdc8f611f5297d686d941afe9e
ubuntu <170117454+coder351@users.noreply.github.com>
2024-08-15 08:03:54 -0400

    use pip to build wheel

--- userspace/compile-pyqt5.sh
+++ userspace/compile-pyqt5.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -e
 
-# rebuildpyqt5=1
-# if [[ "$rebuildpyqt5" -eq "1" ]]; then
-  echo "force rebuild pyqt5"
-  touch ./userspace/files/PyQt5-5.15.9-cp38-abi3-manylinux_2_17_aarch64.whl
-# fi
+# Use pyenv venv
+export PATH="/usr/local/pyenv/bin:/usr/local/pyenv/shims:$PATH"
+export PYENV_ROOT="/usr/local/pyenv"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# Build PyQt5 wheel
+cd /tmp
+wget https://files.pythonhosted.org/packages/5c/46/b4b6eae1e24d9432905ef1d4e7c28b6610e28252527cdc38f2a75997d8b5/PyQt5-5.15.9.tar.gz
+tar xf PyQt5-5.15.9.tar.gz
+cd PyQt5-5.15.9
+
+export MAKEFLAGS="-j$(nproc)"
+pip wheel -w . --verbose --config-settings --confirm-license= .
