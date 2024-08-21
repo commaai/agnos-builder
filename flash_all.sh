49c68541eb08eb5206f83a3d9eb89e4e1c64536b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-08-20 20:08:49 -0700

    download from manifest (#322)

--- flash_all.sh
+++ flash_all.sh
@@ -4,11 +4,12 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-# TODO: download firmware from firmware.json
-#for part in aop abl xbl xbl_config devcfg; do
-#  tools/edl w ${part}_a $DIR/agnos-firmware/$part.bin
-#  tools/edl w ${part}_b $DIR/agnos-firmware/$part.bin
-#done
+DOWNLOADED="aop abl xbl xbl_config devcfg"
+scripts/download-from-manifest.py --master --partitions $DOWNLOADED
+for part in $DOWNLOADED; do
+  tools/edl w ${part}_a $DIR/agnos-firmware/$part.bin
+  tools/edl w ${part}_b $DIR/agnos-firmware/$part.bin
+done
 
 ./flash_kernel.sh
 ./flash_system.sh
