406e94324a21d8f541292554db11e79b39c99166
ubuntu <170117454+coder351@users.noreply.github.com>
2024-08-28 05:05:19 -0400

    remove pyqt5 pre-built wheel

--- userspace/compile-pyqt5.sh
+++ userspace/compile-pyqt5.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -e
 
-# Use pyenv venv
-export PATH="/usr/local/pyenv/bin:/usr/local/pyenv/shims:$PATH"
-export PYENV_ROOT="/usr/local/pyenv"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+source $XDG_DATA_HOME/venv/bin/activate && \
 
 # Build PyQt5 wheel
 cd /tmp
@@ -12,5 +8,4 @@ wget https://files.pythonhosted.org/packages/5c/46/b4b6eae1e24d9432905ef1d4e7c28
 tar xf PyQt5-5.15.9.tar.gz
 cd PyQt5-5.15.9
 
-export MAKEFLAGS="-j$(nproc)"
 pip wheel -w . --verbose --config-settings --confirm-license= .
