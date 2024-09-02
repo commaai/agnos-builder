e31ce23f8dadd86dbcfd19a9bdb88063fd5de9dc
Robbe Derks <robbe.derks@gmail.com>
2024-09-02 15:24:10 +0200

    also copy over sound kernel modules

--- load_kernel.sh
+++ load_kernel.sh
@@ -13,6 +13,8 @@ sudo dd if=/data/tmp/boot.img of=/dev/disk/by-partlabel/boot_b
 sudo mount -o rw,remount /
 sudo resize2fs $(findmnt -n -o SOURCE /)
 sudo mv /data/tmp/wlan.ko /usr/comma/wlan.ko
+sudo mv /data/tmp/snd-soc-sdm845.ko /usr/comma/sound/snd-soc-sdm845.ko
+sudo mv /data/tmp/snd-soc-wcd9xxx.ko /usr/comma/sound/snd-soc-wcd9xxx.ko
 rm -rf /data/tmp/*
 sudo mount -o ro,remount / || true
 sudo reboot
