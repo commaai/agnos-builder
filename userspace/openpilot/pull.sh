5f17285857c9b0fba0dd6bbc920cab22075cafca
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-03 14:38:28 +0300

    check in openpilot pyproject.toml and uv.lock

---
+++ userspace/openpilot/pull.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+curl -sO https://raw.githubusercontent.com/commaai/openpilot/master/pyproject.toml
+curl -sO https://raw.githubusercontent.com/commaai/openpilot/master/uv.lock
