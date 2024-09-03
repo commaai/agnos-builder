03e3a93f6422ca7aee6e85a2b36b82f5f6d813bf
Comma Device <device@comma.ai>
2024-09-03 17:44:14 +0300

    uv lock in sync

--- sync_openpilot_dependencies.sh
+++ sync_openpilot_dependencies.sh
@@ -5,6 +5,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-cd userspace/uv/openpilot
-curl -sO https://raw.githubusercontent.com/commaai/openpilot/master/pyproject.toml
-curl -sO https://raw.githubusercontent.com/commaai/openpilot/master/uv.lock
+cd userspace/uv
+curl -sSo openpilot/pyproject.toml https://raw.githubusercontent.com/commaai/openpilot/master/pyproject.toml
+curl -sSo openpilot/uv.lock https://raw.githubusercontent.com/commaai/openpilot/master/uv.lock
+uv lock
