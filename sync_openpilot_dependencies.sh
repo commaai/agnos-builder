a441ac2a73b6960fa50ae6ff005a512fb131a041
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-03 16:12:14 +0300

    use a uv workspace instead https://docs.astral.sh/uv/concepts/workspaces/

--- sync_openpilot_dependencies.sh
+++ sync_openpilot_dependencies.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+# Make sure we're in the correct spot
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+cd $DIR
+
+cd userspace/uv/openpilot
 curl -sO https://raw.githubusercontent.com/commaai/openpilot/master/pyproject.toml
 curl -sO https://raw.githubusercontent.com/commaai/openpilot/master/uv.lock
