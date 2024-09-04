0a0c7074d74819cc5f76c4d5d48a27ca9b7ef8c0
Comma Device <device@comma.ai>
2024-09-04 16:35:12 +0300

    openpilot uv.lock is now workspace uv.lock

--- sync_openpilot_dependencies.sh
+++ sync_openpilot_dependencies.sh
@@ -7,7 +7,7 @@ cd $DIR
 
 cd userspace/uv
 curl -sSo openpilot/pyproject.toml https://raw.githubusercontent.com/commaai/openpilot/master/pyproject.toml
-curl -sSo openpilot/uv.lock https://raw.githubusercontent.com/commaai/openpilot/master/uv.lock
+curl -sSo uv.lock https://raw.githubusercontent.com/commaai/openpilot/master/uv.lock
 
 export PYOPENCL_CL_PRETEND_VERSION="2.0" && \
 pc="$(python -c "import sysconfig;print(sysconfig.get_config_vars('installed_base')[0])")" && \
