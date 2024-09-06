5bb104da3983a60fe2543f182060576e4e642b8d
Robin Reckmann <robin.reckmann@gmail.com>
2024-09-07 02:30:21 +0900

    Use debhelper tools for dependency compilation and packaging

--- userspace/compile-ffmpeg.sh
+++ userspace/compile-ffmpeg.sh
@@ -6,15 +6,19 @@ wget https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.bz2
 tar xvf ffmpeg-4.2.2.tar.bz2
 cd ffmpeg-4.2.2
 
+export DEBFULLNAME=comma
+export LOGNAME=comma
+
+dh_make --createorig -s -p ffmpeg_4.2.2 -y
+
 # avoid makeinfo: error parsing ./doc/t2h.pm: Undefined subroutine &Texinfo::Config::set_from_init_file called at ./doc/t2h.pm line 24.
 # with --disable-htmlpages
 # --disable-doc works too, disables building documentation completely
 # https://gist.github.com/omegdadi/6904512c0a948225c81114b1c5acb875
 # https://github.com/7Ji/archrepo/issues/10
-./configure --enable-shared --disable-static --disable-htmlpages
-make -j$(nproc)
+echo -e "override_dh_auto_configure:\n\t./configure --enable-shared --disable-static --disable-htmlpages" >> debian/rules
+echo -e "override_dh_usrlocal:" >> debian/rules
 
-# remove "--fstrans=no" when checkinstall is fixed (still not fixed in 24.04)
-# # https://bugs.launchpad.net/ubuntu/+source/checkinstall/+bug/78455
-checkinstall -yD --install=no --fstrans=no --pkgname=ffmpeg
-mv ffmpeg*.deb /tmp/ffmpeg.deb
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -us -uc -nc
+
+mv ../ffmpeg*.deb /tmp/ffmpeg.deb
