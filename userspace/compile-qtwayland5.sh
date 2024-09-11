f856a66bfce0aaeba4ba92dcba0f218c7579cab8
Robin Reckmann <robin.reckmann@gmail.com>
2024-09-12 05:36:48 +0900

    Build independent packages first (#354)
    * Move package compilation to separate dockerfile
    
    * Add tag
    
    * Revert "Add tag"
    
    This reverts commit 6ed675b5db217e3ffaa45711444742127d2d3ee9.
    
    * Export packages from compiler docker
    
    * Add packages folder to gitignore
    
    * Add newline at EOF
    
    * Keep cleaning apt
    
    * Minimize compiler image
    
    * Keep compilation in main dockerfile
    
    * Revert some more
    
    * Remove wip part for now
    
    * Move build requirements installation to build scripts
    
    * Remove newlines
    
    * lint
    
    * Compile and package modemmanager
    
    * Disable unrelated modems
    
    * Add runtime dependencies
    
    * [Upload]
    
    * Add missing package to fix networking
    
    * Move package install to mm for now

--- userspace/compile-qtwayland5.sh
+++ userspace/compile-qtwayland5.sh
@@ -1,9 +1,35 @@
 #!/bin/bash -e
 
-# Patched qtwayland that outputs a fixed screen size
-# Clone qtwayland submodule, checkout, apply patch, qmake, make
 VERSION=5.12.9
 
+# Install build requirements
+dpkg --add-architecture armhf
+
+apt-get update && apt-get install -yq --no-install-recommends \
+    libc6:armhf \
+    libdbus-1-3 \
+    libegl-dev \
+    libexpat1:armhf \
+    libfontconfig-dev \
+    libfreetype6 \
+    libgles-dev \
+    libglib2.0-dev \
+    libharfbuzz-dev \
+    libpcre2-dev \
+    libwayland-dev \
+    libxcomposite-dev \
+    libxkbcommon-dev \
+    zlib1g-dev
+
+apt-get -o Dpkg::Options::="--force-overwrite" install -yq \
+    /tmp/agnos/qt-5.12.8.deb \
+    /tmp/agnos/libwayland-1.9.0-1.deb \
+    /tmp/agnos/libicu66_66.1-2ubuntu2.1_arm64.deb \
+    /tmp/agnos/libssl1.1_1.1.1f-1ubuntu2.22_arm64.deb \
+    /tmp/agnos/libffi6_3.2.1-8_arm64.deb
+
+# Patched qtwayland that outputs a fixed screen size
+# Clone qtwayland submodule, checkout, apply patch, qmake, make
 cd /tmp
 git clone --branch v${VERSION} --depth 1 https://github.com/qt/qtwayland.git
 cd qtwayland
