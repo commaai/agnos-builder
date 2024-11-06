a3bba6b96bb1ad551f183079870496b35ee0e7d9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-11-05 19:13:53 -0800

    add socat

--- userspace/install_extras.sh
+++ userspace/install_extras.sh
@@ -6,7 +6,8 @@ apt-fast update && apt-fast install -y --no-install-recommends \
   bash-completion \
   irqtop \
   ripgrep \
-  nfs-common
+  nfs-common \
+  socat
 
 # color prompt
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/comma/.bashrc
