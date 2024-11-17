b86dee7af9ef2b0e0d1967796124e0311fc8eb08
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-11-17 13:47:43 -0800

    Python 3.13

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -2,11 +2,11 @@
 
 echo "installing uv..."
 
-export XDG_DATA_HOME="/usr/local" 
+export XDG_DATA_HOME="/usr/local"
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-PYTHON_VERSION="3.12.3"
+PYTHON_VERSION="3.13"
 
 # uv requires virtual env either managed or system before installing dependencies
 uv venv $XDG_DATA_HOME/venv --seed --python-preference only-managed --python=$PYTHON_VERSION
