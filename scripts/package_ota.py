fb6e156f2b897a691d93eb1b0684a41dbc532c48
Maxime Desroches <desroches.maxime@gmail.com>
2025-01-24 20:15:20 -0800

    all

--- scripts/package_ota.py
+++ scripts/package_ota.py
@@ -16,6 +16,43 @@ BUILD_DIR = ROOT / "build"
 AGNOS_UPDATE_URL = os.getenv("AGNOS_UPDATE_URL", "https://commadist.azureedge.net/agnosupdate")
 AGNOS_STAGING_UPDATE_URL = os.getenv("AGNOS_STAGING_UPDATE_URL", "https://commadist.azureedge.net/agnosupdate-staging")
 
+# Structure: (lun, partition_name, filename, start_sector, num_sectors)
+PARTITION_TABLES = [
+  (0, 'gpt_main_0', 'gpt_main_0.bin', 0, 6),
+  (1, 'gpt_main_1', 'gpt_main_1.bin', 0, 6),
+  (2, 'gpt_main_2', 'gpt_main_2.bin', 0, 6),
+  (3, 'gpt_main_3', 'gpt_main_3.bin', 0, 6),
+  (4, 'gpt_main_4', 'gpt_main_4.bin', 0, 6),
+  (5, 'gpt_main_5', 'gpt_main_5.bin', 0, 6),
+]
+
+# Structure: (partition_name, filename, has_ab)
+QDL_FLASH_ARRAY = [
+  ('persist', 'persist.bin', False),
+  ('systemrw', 'systemrw.bin', False),
+  ('cache', 'cache.bin', False),
+  ('xbl', 'xbl.bin', True),
+  ('xbl_config', 'xbl_config.bin', True),
+  ('abl', 'abl.bin', True),
+  ('aop', 'aop.bin', True),
+  ('bluetooth', 'bluetooth.bin', True),
+  ('cmnlib64', 'cmnlib64.bin', True),
+  ('cmnlib', 'cmnlib.bin', True),
+  ('devcfg', 'devcfg.bin', True),
+  ('devinfo', 'devinfo.bin', False),
+  ('dsp', 'dsp.bin', True),
+  ('hyp', 'hyp.bin', True),
+  ('keymaster', 'keymaster.bin', True),
+  ('limits', 'limits.bin', False),
+  ('logfs', 'logfs.bin', False),
+  ('modem', 'modem.bin', True),
+  ('qupfw', 'qupfw.bin', True),
+  ('splash', 'splash.bin', False),
+  ('storsec', 'storsec.bin', True),
+  ('tz', 'tz.bin', True),
+]
+
+
 def checksum(fn):
   sha256 = hashlib.sha256()
   with open(fn, 'rb') as f:
@@ -30,7 +67,7 @@ def compress(fin, fout) -> None:
   subprocess.check_call(f"xz -0 -T0 -vc {fin} > {fout}", shell=True)
 
 
-def process_file(fn, name, full_check=True, has_ab=True):
+def process_file(fn, name, full_check=True, has_ab=True, gpt=False, lun=0, start_sector=0, num_sectors=0):
   print(name)
   hash_raw = hash = checksum(fn)
   size = fn.stat().st_size
@@ -59,6 +96,13 @@ def process_file(fn, name, full_check=True, has_ab=True):
     }
     shutil.copy(fn, OTA_OUTPUT_DIR / f"{fn.stem}-{hash_raw}.img")
 
+  if gpt:
+    ret["gpt"] = {
+      "lun": lun,
+      "start_sector": start_sector,
+      "num_sectors": num_sectors,
+    }
+
   return ret
 
 
@@ -69,9 +113,12 @@ if __name__ == "__main__":
     process_file(OUTPUT_DIR / "boot.img", "boot"),
     process_file(OUTPUT_DIR / "system.img", "system", full_check=False),
   ]
-  for fw in ("xbl", "abl", "xbl_config", "devcfg", "aop"):
-    # firmware not built in this repo
-    files.append(process_file(FIRMWARE_DIR / f"{fw}.bin", fw))
+
+  for (name, fn, has_ab) in QDL_FLASH_ARRAY:
+    files.append(process_file(FIRMWARE_DIR / fn, name, has_ab))
+
+  for (lun, name, fn, start_sector, num_sectors) in PARTITION_TABLES:
+    files.append(process_file(FIRMWARE_DIR / fn, name, has_ab=False, gpt=True, lun=lun, start_sector=start_sector, num_sectors=num_sectors))
 
   configs = [
     (AGNOS_UPDATE_URL, "ota.json"),
