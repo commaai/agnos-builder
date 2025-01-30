a00af20d8f3e3c57d19e8f9d6f8b0ff1a6e9c77d
Maxime Desroches <desroches.maxime@gmail.com>
2025-01-29 16:39:17 -0800

    on the fly

--- scripts/package_ota.py
+++ scripts/package_ota.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import io
 import json
 import os
 import hashlib
 import shutil
+import struct
 import subprocess
 from copy import deepcopy
 from pathlib import Path
@@ -14,57 +16,97 @@ FIRMWARE_DIR = ROOT / "firmware"
 OTA_OUTPUT_DIR = OUTPUT_DIR / "ota"
 BUILD_DIR = ROOT / "build"
 
+SECTOR_SIZE = 4096
+
 AGNOS_UPDATE_URL = os.getenv("AGNOS_UPDATE_URL", "https://commadist.azureedge.net/agnosupdate")
 AGNOS_STAGING_UPDATE_URL = os.getenv("AGNOS_STAGING_UPDATE_URL", "https://commadist.azureedge.net/agnosupdate-staging")
 
-GPT = namedtuple('GPT', ['lun', 'name', 'path', 'start_sector', 'num_sectors', 'has_ab', 'ota', 'full_check'])
+GPT = namedtuple('GPT', ['lun', 'name', 'path', 'start_sector', 'num_sectors', 'has_ab', 'ota', 'full_check', 'sparse'])
 GPTS = [
-  GPT(0, 'gpt_main_0', FIRMWARE_DIR / 'gpt_main_0.bin', 0, 6, False, False, True),
-  GPT(1, 'gpt_main_1', FIRMWARE_DIR / 'gpt_main_1.bin', 0, 6, False, False, True),
-  GPT(2, 'gpt_main_2', FIRMWARE_DIR / 'gpt_main_2.bin', 0, 6, False, False, True),
-  GPT(3, 'gpt_main_3', FIRMWARE_DIR / 'gpt_main_3.bin', 0, 6, False, False, True),
-  GPT(4, 'gpt_main_4', FIRMWARE_DIR / 'gpt_main_4.bin', 0, 6, False, False, True),
-  GPT(5, 'gpt_main_5', FIRMWARE_DIR / 'gpt_main_5.bin', 0, 6, False, False, True),
+  GPT(0, 'gpt_main_0', FIRMWARE_DIR / 'gpt_main_0.bin', 0, 6, False, False, True, False),
+  GPT(1, 'gpt_main_1', FIRMWARE_DIR / 'gpt_main_1.bin', 0, 6, False, False, True, False),
+  GPT(2, 'gpt_main_2', FIRMWARE_DIR / 'gpt_main_2.bin', 0, 6, False, False, True, False),
+  GPT(3, 'gpt_main_3', FIRMWARE_DIR / 'gpt_main_3.bin', 0, 6, False, False, True, False),
+  GPT(4, 'gpt_main_4', FIRMWARE_DIR / 'gpt_main_4.bin', 0, 6, False, False, True, False),
+  GPT(5, 'gpt_main_5', FIRMWARE_DIR / 'gpt_main_5.bin', 0, 6, False, False, True, False),
 ]
 
-Partition = namedtuple('Partition', ['name', 'path', 'has_ab', 'ota', 'full_check'])
+Partition = namedtuple('Partition', ['name', 'path', 'has_ab', 'ota', 'full_check', 'sparse'])
 PARTITIONS = [
-  Partition('persist', FIRMWARE_DIR / 'persist.bin', False, False, True),
-  Partition('systemrw', FIRMWARE_DIR / 'systemrw.bin', False, False, True),
-  Partition('cache', FIRMWARE_DIR / 'cache.bin', False, False, True),
-  Partition('xbl', FIRMWARE_DIR / 'xbl.bin', True, True, True),
-  Partition('xbl_config', FIRMWARE_DIR / 'xbl_config.bin', True, True, True),
-  Partition('abl', FIRMWARE_DIR / 'abl.bin', True, True, True),
-  Partition('aop', FIRMWARE_DIR / 'aop.bin', True, True, True),
-  Partition('bluetooth', FIRMWARE_DIR / 'bluetooth.bin', True, False, True),
-  Partition('cmnlib64', FIRMWARE_DIR / 'cmnlib64.bin', True, False, True),
-  Partition('cmnlib', FIRMWARE_DIR / 'cmnlib.bin', True, False, True),
-  Partition('devcfg', FIRMWARE_DIR / 'devcfg.bin', True, True, True),
-  Partition('devinfo', FIRMWARE_DIR / 'devinfo.bin', False, False, True),
-  Partition('dsp', FIRMWARE_DIR / 'dsp.bin', True, False, True),
-  Partition('hyp', FIRMWARE_DIR / 'hyp.bin', True, False, True),
-  Partition('keymaster', FIRMWARE_DIR / 'keymaster.bin', True, False, True),
-  Partition('limits', FIRMWARE_DIR / 'limits.bin', False, False, True),
-  Partition('logfs', FIRMWARE_DIR / 'logfs.bin', False, False, True),
-  Partition('modem', FIRMWARE_DIR / 'modem.bin', True, False, True),
-  Partition('qupfw', FIRMWARE_DIR / 'qupfw.bin', True, False, True),
-  Partition('splash', FIRMWARE_DIR / 'splash.bin', False, False, True),
-  Partition('storsec', FIRMWARE_DIR / 'storsec.bin', True, False, True),
-  Partition('tz', FIRMWARE_DIR / 'tz.bin', True, False, True),
-  Partition('boot', OUTPUT_DIR / 'boot.img', True, False, True),
-  Partition('system', OUTPUT_DIR / 'system.img', True, False, False),
-  Partition('userdata', OUTPUT_DIR / 'userdata_90.img', True, False, True),
-  Partition('userdata', OUTPUT_DIR / 'userdata_89.img', True, False, True),
-  Partition('userdata', OUTPUT_DIR / 'userdata_30.img', True, False, True),
+  Partition('persist', FIRMWARE_DIR / 'persist.bin', False, False, True, False),
+  Partition('systemrw', FIRMWARE_DIR / 'systemrw.bin', False, False, True, False),
+  Partition('cache', FIRMWARE_DIR / 'cache.bin', False, False, True, False),
+  Partition('xbl', FIRMWARE_DIR / 'xbl.bin', True, True, True, False),
+  Partition('xbl_config', FIRMWARE_DIR / 'xbl_config.bin', True, True, True, False),
+  Partition('abl', FIRMWARE_DIR / 'abl.bin', True, True, True, False),
+  Partition('aop', FIRMWARE_DIR / 'aop.bin', True, True, True, False),
+  Partition('bluetooth', FIRMWARE_DIR / 'bluetooth.bin', True, False, True, False),
+  Partition('cmnlib64', FIRMWARE_DIR / 'cmnlib64.bin', True, False, True, False),
+  Partition('cmnlib', FIRMWARE_DIR / 'cmnlib.bin', True, False, True, False),
+  Partition('devcfg', FIRMWARE_DIR / 'devcfg.bin', True, True, True, False),
+  Partition('devinfo', FIRMWARE_DIR / 'devinfo.bin', False, False, True, False),
+  Partition('dsp', FIRMWARE_DIR / 'dsp.bin', True, False, True, False),
+  Partition('hyp', FIRMWARE_DIR / 'hyp.bin', True, False, True, False),
+  Partition('keymaster', FIRMWARE_DIR / 'keymaster.bin', True, False, True, False),
+  Partition('limits', FIRMWARE_DIR / 'limits.bin', False, False, True, False),
+  Partition('logfs', FIRMWARE_DIR / 'logfs.bin', False, False, True, False),
+  Partition('modem', FIRMWARE_DIR / 'modem.bin', True, False, True, False),
+  Partition('qupfw', FIRMWARE_DIR / 'qupfw.bin', True, False, True, False),
+  Partition('splash', FIRMWARE_DIR / 'splash.bin', False, False, True, False),
+  Partition('storsec', FIRMWARE_DIR / 'storsec.bin', True, False, True, False),
+  Partition('tz', FIRMWARE_DIR / 'tz.bin', True, False, True, False),
+  Partition('boot', OUTPUT_DIR / 'boot.img', True, True, True, False),
+  Partition('system', OUTPUT_DIR / 'system.img', True, True, False, True),
+  Partition('userdata', OUTPUT_DIR / 'userdata_90.img', True, False, True, True),
+  Partition('userdata', OUTPUT_DIR / 'userdata_89.img', True, False, True, True),
+  Partition('userdata', OUTPUT_DIR / 'userdata_30.img', True, False, True, True),
 ]
 
 
-def checksum(fn):
+def file_checksum(fn):
   sha256 = hashlib.sha256()
   with open(fn, 'rb') as f:
     for chunk in iter(lambda: f.read(4096), b""):
       sha256.update(chunk)
-  return sha256.hexdigest()
+  return sha256
+
+def ondevice_checksum_sparse(fn):
+  with open(fn, 'rb') as data_source:
+    dat = data_source.read(28)
+    header = struct.unpack("<I4H4I", dat)
+
+    magic = header[0]
+    major_version = header[1]
+    minor_version = header[2]
+    file_hdr_sz = header[3]
+    chunk_hdr_sz = header[4]
+    blk_sz = header[5]
+    total_chunks = header[7]
+
+    assert magic == 0xED26FF3A
+    assert major_version == 1 and minor_version == 0
+    assert file_hdr_sz == 28
+    assert chunk_hdr_sz == 12
+    assert blk_sz == SECTOR_SIZE
+
+    sha256 = hashlib.sha256()
+    for _ in range(total_chunks):
+      header_bin = data_source.read(12)
+
+      header = struct.unpack("<2H2I", header_bin)
+      chunk_type = header[0]
+      chunk_sz = header[2]
+
+      if chunk_type == 0xCAC1: # RAW
+        sha256.update(data_source.read(chunk_sz * SECTOR_SIZE))
+      elif chunk_type == 0xCAC2: # FILL
+        sha256.update(data_source.read(4) * (chunk_sz * SECTOR_SIZE // 4))
+      elif chunk_type == 0xCAC3: # DONT_CARE
+        pass
+      else:
+        raise Exception(f'UNKNOWN SPARSE CHUNK: {hex(chunk_type)}')
+
+    return sha256
 
 def compress(fin, fout) -> None:
   # since system.img is a squashfs now, we don't rely on this compression.
@@ -74,10 +116,17 @@ def compress(fin, fout) -> None:
 
 
 def process_file(entry):
-  print(entry.name)
-  hash_raw = hash = checksum(entry.path)
   size = entry.path.stat().st_size
-  print(f"  {size} bytes, hash {hash}")
+  print(f"\n{entry.name} {size} bytes")
+
+  sha256 = file_checksum(entry.path)
+  hash = hash_raw = sha256.hexdigest()
+
+  if struct.unpack("<I", open(entry.path, 'rb').peek(4)[:4])[0] == 0xED26FF3A:
+    sha256 = ondevice_checksum_sparse(entry.path)
+  elif (size % SECTOR_SIZE) != 0:
+    sha256.update(b'\x00' * ((SECTOR_SIZE - (size % SECTOR_SIZE)) % SECTOR_SIZE))
+  ondevice_hash = sha256.hexdigest()
 
   print("  compressing")
   xz_fn = OTA_OUTPUT_DIR / f"{entry.path.stem}-{hash_raw}.img.xz"
@@ -89,9 +138,10 @@ def process_file(entry):
     "hash": hash,
     "hash_raw": hash_raw,
     "size": size,
-    "sparse": False,
+    "sparse": entry.sparse,
     "full_check": entry.full_check,
     "has_ab": entry.has_ab,
+    "ondevice_hash": ondevice_hash,
   }
 
   if entry.name == "system":
@@ -101,6 +151,11 @@ def process_file(entry):
       "size": size,
     }
     shutil.copy(entry.path, OTA_OUTPUT_DIR / f"{entry.path.stem}-{hash_raw}.img")
+
+  if entry.name == "userdata":
+    ret["userdata"] = {
+      "type": entry.path.with_suffix('').name,
+    }
 
   if isinstance(entry, GPT):
     ret["gpt"] = {
