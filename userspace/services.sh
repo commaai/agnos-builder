f10a501e34f99d8f202306013292b15d64eb1b33
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-02-03 17:22:57 -0800

    and another time bug

--- userspace/services.sh
+++ userspace/services.sh
@@ -22,7 +22,7 @@ systemctl enable ssh-param-watcher.path
 systemctl enable ssh-param-watcher.service
 systemctl enable adb-param-watcher.path
 systemctl enable adb-param-watcher.service
-systemctl enable logrotate-hourly.timer
+systemctl enable logrotate-minutely.timer
 
 # Disable some of our services
 systemctl disable agnos-tests.service
