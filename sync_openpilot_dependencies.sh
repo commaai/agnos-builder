d9876c87d31283d6f4c78765c1a58276ba62f163
Andrei Radulescu <andi.radulescu@gmail.com>
2025-05-06 09:43:05 +0300

    use python3 in sync_openpilot_dependencies.sh

--- sync_openpilot_dependencies.sh
+++ sync_openpilot_dependencies.sh
@@ -9,7 +9,7 @@ cd userspace/uv
 curl -sSo openpilot/pyproject.toml https://raw.githubusercontent.com/commaai/openpilot/master/pyproject.toml
 
 export PYOPENCL_CL_PRETEND_VERSION="2.0" && \
-pc="$(python -c "import sysconfig;print(sysconfig.get_config_vars('installed_base')[0])")" && \
+pc="$(python3 -c "import sysconfig;print(sysconfig.get_config_vars('installed_base')[0])")" && \
 pcpath=$pc"/lib/pkgconfig" && \
 export PKG_CONFIG_PATH="$pcpath:$PKG_CONFIG_PATH" && \
 uv lock --upgrade
