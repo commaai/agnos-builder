f396d0eb1e152c0ce9bb36ad4bf4c2f74af4a79b
Trey Moen <50057480+greatgitsby@users.noreply.github.com>
2025-05-20 14:43:41 -0700

    feat: update libqmi, add lpac (#472)
    * feat: update libqmi, add lpac
    
    * eof

--- userspace/compile-modemmanager.sh
+++ userspace/compile-modemmanager.sh
@@ -2,35 +2,7 @@
 set -e
 
 MM_VERSION="1.22.0"
-LIBQMI_VERSION="1.34.0"
 
-cd /tmp
-
-# meson support for checkinstall
-git clone https://github.com/keithbowes/meson-install.git
-
-# libqmi
-apt-get update && apt-get install -yq --no-install-recommends \
-      bash-completion \
-      gobject-introspection \
-      gtk-doc-tools \
-      help2man \
-      libgirepository1.0-dev \
-      libglib2.0-dev \
-      libgudev-1.0-dev \
-      meson \
-      ninja-build \
-
-git clone -b $LIBQMI_VERSION --depth 1 https://gitlab.freedesktop.org/mobile-broadband/libqmi.git
-cd libqmi
-meson setup build --prefix=/usr --libdir=/usr/lib/aarch64-linux-gnu -Dmbim_qmux=false -Dqrtr=false
-ninja -C build
-
-cd build
-checkinstall -yD --install=no --fstrans=no --pkgname=libqmi /tmp/meson-install/meson-install
-mv libqmi*.deb /tmp/libqmi.deb
-
-# ModemManager
 cd /tmp
 
 git clone -b $MM_VERSION --depth 1 https://gitlab.freedesktop.org/mobile-broadband/ModemManager.git
@@ -42,8 +14,6 @@ apt-get install -y --no-install-recommends \
       libpolkit-gobject-1-dev \
       libsystemd-dev \
       udev
-
-apt-get -o Dpkg::Options::="--force-overwrite" install -yq /tmp/libqmi.deb
 
 cd ModemManager
 meson setup build \
