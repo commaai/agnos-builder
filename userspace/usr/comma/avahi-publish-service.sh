2ba203c2f9c0082e981e12e708d2bbf661f14d8c
Brett Sanderson <l-brett-l@hotmail.com>
2025-06-24 00:58:00 -0400

    avahi publish

---
+++ userspace/usr/comma/avahi-publish-service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Read the raw device model
+RAW_MODEL=$(tr -d '\0' < /sys/firmware/devicetree/base/model)
+
+# Extract the device model (e.g., tici, tizi, mici)
+MODEL=$(echo "$RAW_MODEL" | sed 's/comma //g' | awk '{print $1}')
+
+[ -z "$MODEL" ] && MODEL="comma-unknown"
+
+HOSTNAME=$(hostname)
+SERVICE_NAME="openpilot SSH - $MODEL - [$HOSTNAME]"
+
+# Publish avahi service
+exec avahi-publish -s "$SERVICE_NAME" _ssh._tcp 22 "vendor=comma" "device=openpilot" "model=$MODEL"
