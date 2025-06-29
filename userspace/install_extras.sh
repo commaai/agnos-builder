715281cfa99e1de70859e61464ee8992e9b424f0
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-06-29 14:57:56 -0700

    extras: add wavemon

--- userspace/install_extras.sh
+++ userspace/install_extras.sh
@@ -12,7 +12,8 @@ apt-fast update && apt-fast install -y --no-install-recommends \
   ripgrep \
   ncdu \
   nfs-common \
-  socat
+  socat \
+  wavemon
 
 # color prompt
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/comma/.bashrc
