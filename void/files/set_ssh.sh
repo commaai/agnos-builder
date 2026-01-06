9f26393f865b6bfc476a4176be0d85b192e063f4
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-01-05 19:00:13 -0800

    ssh always on for now

--- void/files/set_ssh.sh
+++ void/files/set_ssh.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Void Linux version - uses sv instead of systemctl
 
-SSH_PARAM="/data/params/d/SshEnabled"
-if [ -f "$SSH_PARAM" ] && [ "$(< $SSH_PARAM)" == "1" ]; then
-  echo "Enabling SSH"
-  sv up sshd
-else
-  echo "Disabling SSH"
-  sv down sshd
-fi
+# During development: always enable SSH
+# TODO: Re-enable param check for production
+echo "Enabling SSH"
+sv up sshd
+
+# SSH_PARAM="/data/params/d/SshEnabled"
+# if [ -f "$SSH_PARAM" ] && [ "$(< $SSH_PARAM)" == "1" ]; then
+#   echo "Enabling SSH"
+#   sv up sshd
+# else
+#   echo "Disabling SSH"
+#   sv down sshd
+# fi
