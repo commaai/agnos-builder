4e1a5b761adb775ba7c99b18366f1086329086d0
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-03-04 11:40:54 -0800

    revert raylib

--- userspace/openpilot_dependencies.sh
+++ userspace/openpilot_dependencies.sh
@@ -9,19 +9,15 @@ apt-fast install --no-install-recommends -yq \
     casync \
     clang \
     curl \
-    gpiod \
     libarchive-dev \
-    libass-dev \
     libcurl4-openssl-dev \
     libffi-dev \
     libfreetype6-dev \
     libglfw3-dev \
     libglib2.0-0t64 \
     liblzma-dev \
-    libomp-dev \
     libportaudio2 \
     libsdl2-dev \
-    libsqlite3-dev \
     libtool \
     libusb-1.0-0-dev \
     libuv1-dev \
@@ -33,5 +29,4 @@ apt-fast install --no-install-recommends -yq \
     portaudio19-dev \
     texinfo \
     wget \
-    xvfb \
     zlib1g-dev
