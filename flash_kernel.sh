1338f3ffc9752d675a16339f36e4fd727ef9db22
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-03-08 14:32:52 -0700

    works like fastboot now

--- flash_kernel.sh
+++ flash_kernel.sh
@@ -4,16 +4,4 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-echo "Checking active slot..."
-ACTIVE_SLOT=$(tools/qdl getactiveslot)
-
-if [[ "$ACTIVE_SLOT" != "a" && "$ACTIVE_SLOT" != "b" ]]; then
-  echo "Invalid active slot: '$ACTIVE_SLOT'"
-  exit 1
-fi
-
-echo "Active slot: $ACTIVE_SLOT"
-echo "Flashing boot_$ACTIVE_SLOT..."
-tools/qdl flash boot_$ACTIVE_SLOT $DIR/output/boot.img
-
-echo "Flashed boot_$ACTIVE_SLOT!"
+tools/qdl flash boot $DIR/output/boot.img
--- flash_system.sh
+++ flash_system.sh
@@ -4,18 +4,5 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-echo "Checking active slot..."
-ACTIVE_SLOT=$(tools/qdl getactiveslot)
-
-if [[ "$ACTIVE_SLOT" != "a" && "$ACTIVE_SLOT" != "b" ]]; then
-  echo "Invalid active slot: '$ACTIVE_SLOT'"
-  exit 1
-fi
-
-echo "Active slot: $ACTIVE_SLOT"
-echo "Flashing system_$ACTIVE_SLOT..."
-tools/qdl flash system_$ACTIVE_SLOT $DIR/output/system.img
-
+tools/qdl flash system $DIR/output/system.img
 tools/qdl reset
-
-echo "Flashed system_$ACTIVE_SLOT!"
