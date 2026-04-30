028eb08986e8dc49ecbb70d08ee42be8bf2a7b53
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-04-30 11:38:12 -0700

    Reapply "Remove Qt + weston (#502)"
    This reverts commit 9d57291038187fd4108d22eec1d6ae908e432106.

--- userspace/usr/comma/power_drop_monitor.py
+++ userspace/usr/comma/power_drop_monitor.py
@@ -95,7 +95,7 @@ def perform_controlled_shutdown():
 
   # Kill services that draw a lot of power
   printk("Killing services")
-  subprocess.call(["/usr/bin/systemctl", "kill", "--signal=9", "weston", "comma"])
+  subprocess.call(["/usr/bin/systemctl", "kill", "--signal=9", "comma"])
   set_screen_power(False)
 
   printk("Halt")
