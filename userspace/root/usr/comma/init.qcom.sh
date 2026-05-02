c5bc06b95116e81316cd77bd63f0b823bcf4589e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-05-01 18:45:55 -0700

    ipa

--- userspace/root/usr/comma/init.qcom.sh
+++ userspace/root/usr/comma/init.qcom.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # centralized qualcomm init
+# *************************
 
 # don't restart whole SoC on subsystem crash
 for i in {0..7}; do
@@ -11,10 +12,12 @@ done
 echo 2649600 | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
 echo 2649600 | sudo tee /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
 
-# *** wifi ***
 # boot wifi
 echo 1 | sudo tee /sys/kernel/boot_wlan/boot_wlan
 /usr/bin/irsc_util /etc/sec_config
 
 # cdsp
 echo 1 > /sys/kernel/boot_cdsp/boot
+
+# ipa
+echo 1 > /dev/ipa
