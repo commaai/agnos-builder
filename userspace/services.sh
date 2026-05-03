5f5e3d97d113e1e121335b8bbc1673b83ddea071
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-05-02 20:55:24 -0700

    remove power drop monitor

--- userspace/services.sh
+++ userspace/services.sh
@@ -12,7 +12,6 @@ systemctl enable magic.service
 systemctl enable init-qcom.service
 systemctl enable varwatch.service
 systemctl enable power_monitor.service
-systemctl enable power_drop_monitor.service
 systemctl enable brightnessd.service
 systemctl enable ssh-param-watcher.path
 systemctl enable ssh-param-watcher.service
