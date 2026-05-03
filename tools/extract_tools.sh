bad55f5e0e8ab8679dff2aa915bbbb4ca4ae681a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-05-03 14:55:30 -0700

    rm more unused

--- tools/extract_tools.sh
+++ tools/extract_tools.sh
@@ -22,31 +22,13 @@ git lfs ls-files | awk '{print $2}' | grep "-" &>/dev/null && {
 }
 
 LINARO_GCC=aarch64-linux-gnu-gcc
-GOOGLE_GCC_4_9=aarch64-linux-android-4.9
-EDK2_LLVM=llvm-arm-toolchain-ship
-SEC_IMAGE=SecImage
 
 LINARO_GCC_TARBALL=$LINARO_GCC.tar.gz
-GOOGLE_GCC_4_9_TARBALL=$GOOGLE_GCC_4_9.tar.gz
-EDK2_LLVM_TARBALL=$EDK2_LLVM.tar.gz
-SEC_IMAGE_TARBALL=$SEC_IMAGE.tar.gz
 
 ARCH=$(uname -m)
 if [ "$ARCH" != "arm64" ] && [ "$ARCH" != "aarch64" ]; then
   echo "Extracting tools..."
   if [ ! -d $LINARO_GCC ]; then
     tar -xzf $LINARO_GCC_TARBALL &>/dev/null
-  fi
-
-  if [ ! -d $GOOGLE_GCC_4_9 ]; then
-    tar -xzf $GOOGLE_GCC_4_9_TARBALL &>/dev/null
-  fi
-
-  if [ ! -d $EDK2_LLVM ]; then
-    tar -xzf $EDK2_LLVM_TARBALL &>/dev/null
-  fi
-
-  if [ ! -d $SEC_IMAGE ]; then
-    tar -xzf $SEC_IMAGE_TARBALL &>/dev/null
   fi
 fi
