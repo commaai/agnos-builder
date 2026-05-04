bc02ae86ca121597a151d31e9437d934a427c788
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-05-04 09:05:17 -0700

    merge hw init

---
+++ userspace/root/usr/comma/hardware-init.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+# *** gpio init ***
+echo "initializing gpio"
+function gpio {
+  echo "out" > /sys/class/gpio/gpio$1/direction
+  echo $2 > /sys/class/gpio/gpio$1/value
+}
+
+pins=(
+30  # HUB_RST_N
+49  # SOM_ST_IO
+134 # ST_BOOT0
+41  # PANDA_1V8_EN_N
+50  # LTE_RST_N
+116 # LTE_PWRKEY
+124 # ST_RST_N
+34  # GPS_PWR_EN
+33  # GPS_SAFEBOOT_N
+32  # GPS_RST_N
+52  # LTE_BOOT
+1264  # POWER ALERT
+)
+
+for p in ${pins[@]}; do
+  echo $p
+
+  # this is SSD_3v3 EN on tici
+  if [ "$p" -eq 41 ] && grep -q "comma tici" /sys/firmware/devicetree/base/model; then
+    echo "Skipping $p"
+    continue
+  fi
+
+  echo $p > /sys/class/gpio/export
+  until [ -d /sys/class/gpio/gpio$p ]
+  do
+    sleep .05
+  done
+done
+
+
+HUB_RST_N=30
+gpio $HUB_RST_N 1
+
+# *** sound init ***
+echo "waiting for sound card to come online"
+while [ ! -d /proc/asound/sdm845tavilsndc ] || [ "$(cat /proc/asound/card0/state 2> /dev/null)" != "ONLINE" ] ; do
+  sleep 0.01
+done
+echo "sound card online"
+
+while ! /usr/comma/sound/tinymix controls | grep -q "SEC_MI2S_RX Audio Mixer MultiMedia1"; do
+  sleep 0.01
+done
+echo "tinymix controls ready"
+
+/usr/comma/sound/tinymix set "SEC_MI2S_RX Audio Mixer MultiMedia1" 1
+if grep -q mici /sys/firmware/devicetree/base/model; then
+  /usr/comma/sound/tinymix set "MultiMedia1 Mixer SEC_MI2S_TX" 1
+else
+  /usr/comma/sound/tinymix set "MultiMedia1 Mixer TERT_MI2S_TX" 1
+  /usr/comma/sound/tinymix set "TERT_MI2S_TX Channels" Two
+fi
+
+# *** screen calibration ***
+/usr/comma/screen_calibration.py
