a19476d0e341d8c56b0f81badf8debc4b794ede3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-05-04 09:31:48 -0700

    parallel

--- userspace/root/usr/comma/hardware-init.sh
+++ userspace/root/usr/comma/hardware-init.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-set -e
-
-# *** gpio init ***
-echo "initializing gpio"
-function gpio {
-  echo "out" > /sys/class/gpio/gpio$1/direction
-  echo $2 > /sys/class/gpio/gpio$1/value
-}
 
 pins=(
 49  # SOM_ST_IO
@@ -22,34 +14,50 @@ pins=(
 1264  # POWER ALERT
 )
 
-for p in ${pins[@]}; do
-  echo $p
+function init_gpio {
+  echo "initializing gpio"
 
-  echo $p > /sys/class/gpio/export
-  until [ -d /sys/class/gpio/gpio$p ]; do
-    sleep .05
+  for p in ${pins[@]}; do
+    echo $p
+
+    echo $p > /sys/class/gpio/export
+    until [ -d /sys/class/gpio/gpio$p ]; do
+      sleep .05
+    done
   done
-done
+}
 
-# *** sound init ***
-echo "waiting for sound card to come online"
-while [ ! -d /proc/asound/sdm845tavilsndc ] || [ "$(cat /proc/asound/card0/state 2> /dev/null)" != "ONLINE" ] ; do
-  sleep 0.01
-done
-echo "sound card online"
+function init_sound {
+  echo "waiting for sound card to come online"
+  while [ ! -d /proc/asound/sdm845tavilsndc ] || [ "$(cat /proc/asound/card0/state 2> /dev/null)" != "ONLINE" ] ; do
+    sleep 0.01
+  done
+  echo "sound card online"
 
-while ! /usr/comma/sound/tinymix controls | grep -q "SEC_MI2S_RX Audio Mixer MultiMedia1"; do
-  sleep 0.01
-done
-echo "tinymix controls ready"
+  while ! /usr/comma/sound/tinymix controls | grep -q "SEC_MI2S_RX Audio Mixer MultiMedia1"; do
+    sleep 0.01
+  done
+  echo "tinymix controls ready"
 
-/usr/comma/sound/tinymix set "SEC_MI2S_RX Audio Mixer MultiMedia1" 1
-if grep -q mici /sys/firmware/devicetree/base/model; then
-  /usr/comma/sound/tinymix set "MultiMedia1 Mixer SEC_MI2S_TX" 1
-else
-  /usr/comma/sound/tinymix set "MultiMedia1 Mixer TERT_MI2S_TX" 1
-  /usr/comma/sound/tinymix set "TERT_MI2S_TX Channels" Two
-fi
+  /usr/comma/sound/tinymix set "SEC_MI2S_RX Audio Mixer MultiMedia1" 1
+  if grep -q mici /sys/firmware/devicetree/base/model; then
+    /usr/comma/sound/tinymix set "MultiMedia1 Mixer SEC_MI2S_TX" 1
+  else
+    /usr/comma/sound/tinymix set "MultiMedia1 Mixer TERT_MI2S_TX" 1
+    /usr/comma/sound/tinymix set "TERT_MI2S_TX Channels" Two
+  fi
+}
 
-# *** screen calibration ***
-/usr/comma/screen_calibration.py
+function init_screen_calibration {
+  while ! mountpoint -q /persist; do
+    sleep 0.1
+  done
+
+  /usr/comma/screen_calibration.py
+}
+
+init_gpio &
+init_sound &
+init_screen_calibration &
+
+wait
