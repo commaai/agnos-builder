43513c3823107d509c212df2cbe38819c5384c1d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-05-03 20:36:09 -0700

    just one openpilot

--- userspace/openpilot_dependencies.sh
+++ userspace/openpilot_dependencies.sh
@@ -12,12 +12,10 @@ apt-fast install --no-install-recommends -yq \
     gpiod \
     libarchive-dev \
     libcurl4-openssl-dev \
-    libczmq-dev \
     libdbus-1-dev \
     libffi-dev \
     libfreetype6-dev \
     libglib2.0-0t64 \
-    libi2c-dev \
     liblzma-dev \
     libomp-dev \
     libportaudio2 \
@@ -25,6 +23,14 @@ apt-fast install --no-install-recommends -yq \
     libusb-1.0-0-dev \
     libuv1-dev \
     locales \
-    pkg-config \
-    wget \
     zlib1g-dev
+
+echo "installing uv..."
+
+export XDG_DATA_HOME="/usr/local"
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# uv requires virtual env either managed or system before installing dependencies
+PYTHON_VERSION="3.12"
+uv venv $XDG_DATA_HOME/venv --seed --python-preference only-system --python=$PYTHON_VERSION
