899b63f696c5e3d8c88cb6cd47694cdfafcb1705
Himanshu Mor <himanshumor@outlook.in>
2026-07-14 03:14:17 +0530

    added mtp to commaai

--- userspace/base_setup.sh
+++ userspace/base_setup.sh
@@ -80,7 +80,8 @@ apt-fast install --no-install-recommends -yq \
     wget \
     wireless-tools \
     wpasupplicant \
-    zlib1g-dev
+    zlib1g-dev  \
+    umtprd
 
 # Enable serial console on UART
 systemctl enable serial-getty@ttyS0.service
--- userspace/root/usr/comma/set_adb.sh
+++ userspace/root/usr/comma/set_adb.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
 setup() {
-  # Check if /config is already mounted
-  if ! mountpoint -q /config; then
-    sudo mount -t configfs none /config
-  else
-    echo "/config is already mounted."
-  fi
-
-  # Create USB gadget directory structure
-  sudo mkdir -p /config/usb_gadget/g1
   cd /config/usb_gadget/g1
-  sudo mkdir -p strings/0x409
-  sudo mkdir -p configs/c.1/strings/0x409
-  sudo mkdir -p functions/ncm.0
-
-  # Set Vendor and Product ID
-  echo 0x04D8 | sudo tee idVendor
-  echo 0x1234 | sudo tee idProduct
-
-  # Set strings
-  echo "$(cat /proc/cmdline | sed -e 's/^.*androidboot.serialno=//' -e 's/ .*$//')" | sudo tee strings/0x409/serialnumber
-  echo "comma.ai" | sudo tee strings/0x409/manufacturer
-  echo "Linux USB Gadget" | sudo tee strings/0x409/product
-  echo 250 | sudo tee configs/c.1/MaxPower
 
   # Create ADB function
   sudo mkdir -p functions/ffs.adb
@@ -34,11 +12,7 @@ setup() {
     echo "/dev/usb-ffs/adb is already mounted"
   fi
 
-  # Link both functions to configuration
-  echo "NCM+ADB" | sudo tee configs/c.1/strings/0x409/configuration
-  sudo rm -f configs/c.1/ncm.0
-  sudo rm -f configs/c.1/ffs.adb
-  sudo ln -s functions/ncm.0 configs/c.1/
+  # Link ADB functions to configuration
   sudo ln -s functions/ffs.adb configs/c.1/
 }
 
@@ -46,6 +20,7 @@ start() {
   setprop service.adb.tcp.port -1
 
   cd /config/usb_gadget/g1
+  echo "" | sudo tee UDC
   echo "a600000.dwc3" | sudo tee UDC
 }
 
@@ -53,6 +28,8 @@ stop() {
   if [ -d "/config/usb_gadget/g1" ]; then
     cd /config/usb_gadget/g1
     echo "" | sudo tee UDC
+    sudo rm -f configs/c.1/ffs.adb
+    echo "a600000.dwc3" | sudo tee UDC
   fi
 }
 
